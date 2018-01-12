ca7bd10444df2437b4b9e3d9cd5404906fe90e64
James Ross <git@james-ross.co.uk>
2018-01-12 18:58:06 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace daily_news_feed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
