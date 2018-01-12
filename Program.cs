6685097e85baa663d54e9929eaf7d0b89aca5d36
James Ross <git@james-ross.co.uk>
2018-01-12 19:20:08 +0000

    Add packages, configuration and storage

--- Program.cs
+++ Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
-namespace daily_news_feed
+namespace DailyNewsFeed
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync(args).Wait();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            try
+            {
+                var configuration = LoadConfiguration(args);
+                var storage = await LoadStorage(configuration);
+
+                storage.Close();
+            }
+            catch (CommandLineParser.Exceptions.CommandLineException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static IConfigurationRoot LoadConfiguration(string[] args)
+        {
+            var config = new CommandLineParser.Arguments.FileArgument('c', "config")
+            {
+                DefaultValue = new FileInfo("config.json")
+            };
+
+            var commandLineParser = new CommandLineParser.CommandLineParser()
+            {
+                Arguments = {
+                    config,
+                }
+            };
+
+            commandLineParser.ParseCommandLine(args);
+
+            return new ConfigurationBuilder()
+                .AddJsonFile(config.Value.FullName, true)
+                .Build();
+        }
+
+        static async Task<Storage> LoadStorage(IConfigurationRoot configuration)
+        {
+            var storage = new Storage(configuration.GetConnectionString("Storage"));
+            await storage.Open();
+            return storage;
         }
     }
 }
---
+++ Storage.cs
@@ -0,0 +1,54 @@
+using System.Data.Common;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace DailyNewsFeed
+{
+    public class Storage
+    {
+        DbConnection Connection;
+
+        public Storage(string connectionString)
+        {
+            Connection = new SqliteConnection();
+            Connection.ConnectionString = connectionString;
+        }
+
+        public async Task Open()
+        {
+            await Connection.OpenAsync();
+        }
+
+        public void Close()
+        {
+            Connection.Close();
+        }
+
+        public async Task ExecuteNonQueryAsync(string commandText, params object[] parameters)
+        {
+            await CreateCommand(commandText, parameters).ExecuteNonQueryAsync();
+        }
+
+        public async Task<object> ExecuteScalarAsync(string commandText, params object[] parameters)
+        {
+            return await CreateCommand(commandText, parameters).ExecuteScalarAsync();
+        }
+
+        private DbCommand CreateCommand(string commandText, object[] parameters)
+        {
+            var command = Connection.CreateCommand();
+
+            command.CommandText = commandText;
+
+            for (var i = 0; i < parameters.Length; i++)
+            {
+                var param = command.CreateParameter();
+                param.ParameterName = $"@Param{i}";
+                param.Value = parameters[i];
+                command.Parameters.Add(param);
+            }
+
+            return command;
+        }
+    }
+}
